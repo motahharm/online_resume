17f8caa24382018ee9686c86ce88311dfc9dcc7f
motahhar <motahharmokfi@gmail.com>
2021-08-30 23:45:47 +0430

    add header.html and profile_list(not limit)

--- web/urls.py
+++ web/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.index_view),
+  path('', views.index_view, name='home'),
+  path('profile/list/', views.list_profile_view, name='profile_list'),
   path('resume/<id_user>', views.resume_view),
 ]
--- web/views.py
+++ web/views.py
@@ -8,3 +8,8 @@ def resume_view(request, id_user):
   profile =  models.resume_model.objects.get(id=id_user)
   context = {'profile':profile}
   return render(request, 'resume.html', context)
+
+def list_profile_view(request):
+  profiles = models.resume_model.objects.all
+  context = {'profiles':profiles}
+  return render(request, 'list_profile.html', context)
